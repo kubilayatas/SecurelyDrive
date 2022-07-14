1a505eadf4ff7fbcdbb261029bf1bb73863062d4
Kubilay ATAŞ <50429061+kubilayatas@users.noreply.github.com>
2022-07-14 22:43:23 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python ~/SecurelyDrive/detect_drvgz.py --weights ObjectDetectorModel.pt --img 416 --conf 0.4 --drv-gaze --source 0 --name TestDetection --project Test
-sleep 30s
+python ~/SecurelyDrive/detect_drvgz.py --weights ObjectDetectorModel.pt --img 416 --conf 0.4 --drv-gaze --source 0
